d774c93f414c91a56df7c31d5307f6acac04bd00
戚雨 <yuqi@wisedu.com>
2018-05-07 11:45:26 +0800

    允许直接加载模型

--- src/DataSource.js
+++ src/DataSource.js
@@ -1,7 +1,14 @@
 export class DataSource{
     static _ds = {};
     constructor(name, url){
-        this.url = url;
+        if (typeof(url) === "string") {
+            this.url = url;
+        } else if (typeof(url) === "object") {
+            this.meta = url;
+        } else {
+            throw "第二个参数应为url字符串，或者是模型的json"
+        }
+        
         this.inst = undefined;
         this.name = name;
     }
