febcad65a165eac25e74ad793776dad456fc24ee
xujiabin <xujiabin0114@163.com>
2018-05-10 17:04:18 +0800

    增加tg-img标签

---
+++ src/components/tg-img.vue
@@ -0,0 +1,16 @@
+<template>
+  <img @click="handleClick" class="tg-img" />
+</template>
+<script>
+    export default {
+        name: 'tg-img',
+        methods: {
+            handleClick(evt) {
+                this.$emit('click', evt);
+            }
+        }
+    };
+</script>
+<style>
+
+</style>
--- src/entry.js
+++ src/entry.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 import {DataAdapter} from './DataBind/DataAdapter.js';
 import {DataSource} from './DataBind/DataSource.js';
 import TgDiv from './components/tg-div.vue';
+import TgImg from './components/tg-img.vue';
 import TgLinkbutton from './components/tg-linkbutton.vue';
 import TgListview from './components/tg-listview.vue';
 import TgText from './components/tg-text.vue';
@@ -11,7 +12,7 @@ const version = "1.0.0";
 const description = "turing components";
 const author = "金智教育 wisedu";
 const components = {
-    TgDiv,TgLinkbutton,TgListview,TgText
+    TgDiv,TgImg,TgLinkbutton,TgListview,TgText
 };
 const install = function (Vue, opts = {}) {
     if (install.installed) return;
