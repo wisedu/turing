7c2e1444ffe2ca038568cc23c684ffdae5b52ca4
戚雨 <yuqi@wisedu.com>
2018-05-12 08:37:32 +0800

    1.0.4

--- src/entry.js
+++ src/entry.js
@@ -9,7 +9,7 @@ import TgImg from './components/tg-img.vue';
 import TgLinkbutton from './components/tg-linkbutton.vue';
 import TgListview from './components/tg-listview.vue';
 import TgText from './components/tg-text.vue';
-const version = "1.0.1";
+const version = "1.0.4";
 const description = "turing components";
 const author = "金智教育 wisedu";
 const components = {
@@ -28,5 +28,5 @@ if (typeof window !== 'undefined' && window.Vue) {
 export default Object.assign({}, {
     install, 
     ...components,
-    utils, version, description, author, DataAdapter, DataSource, axios, moment
+    utils, version, description, author, DataAdapter, DataSource, iviewAdapter, axios, moment
 });
