c54fd329bf6e54acd946f33da4e514c754380499
戚雨 <yuqi@wisedu.com>
2018-05-29 21:04:55 +0800

    增加模型驱动

---
+++ components/iview-md-form.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Form :model="formValue" :label-width="labelWidth">
+            <tg-listview :datas="model" :grid="{gutter:0, column:column}">
+                <template slot="beforeTemplate">
+                    <slot name="before"></slot>
+                </template>
+                <template slot="itemTemplate" slot-scope="props">
+                    
+                </template>
+                <template slot="afterTemplate">
+                    <slot name="after"></slot>
+                </template>
+            </tg-listview>
+        </Form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- entry.js
+++ entry.js
@@ -4,16 +4,18 @@ import moment from 'moment';
 import iviewAdapter from './ComAdapter/iviewAdapter.js';
 import {DataAdapter} from './DataBind/DataAdapter.js';
 import {DataSourceManager} from './DataBind/DataSourceManager.js';
+import {ModelDrivenFormItem} from './components/ModelDrivenFormItem.js';
+import IviewMdForm from './components/iview-md-form.vue';
 import TgDiv from './components/tg-div.vue';
 import TgImg from './components/tg-img.vue';
 import TgLinkbutton from './components/tg-linkbutton.vue';
 import TgListview from './components/tg-listview.vue';
 import TgText from './components/tg-text.vue';
-const version = "1.1.5";
+const version = "1.1.6";
 const description = "turing components";
 const author = "金智教育 wisedu";
 const components = {
-    TgDiv,TgImg,TgLinkbutton,TgListview,TgText
+    IviewMdForm,TgDiv,TgImg,TgLinkbutton,TgListview,TgText
 };
 const install = function (Vue, opts = {}) {
     if (install.installed) return;
@@ -28,8 +30,8 @@ if (typeof window !== 'undefined' && window.Vue) {
 export default Object.assign({}, {
     install, 
     ...components,
-    utils, version, description, author, DataAdapter, DataSourceManager, iviewAdapter, axios, moment
+    utils, version, description, author, DataAdapter, DataSourceManager, iviewAdapter, ModelDrivenFormItem, axios, moment
 });
 export {
-    utils, version, description, author, DataAdapter, DataSourceManager, iviewAdapter, axios, moment
+    utils, version, description, author, DataAdapter, DataSourceManager, iviewAdapter, ModelDrivenFormItem, axios, moment
 }
