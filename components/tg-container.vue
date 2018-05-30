b19d5e7dcb228e3bea08bccb977246a31eda4e7d
guduziye <05123875lgh@163.com>
2018-05-30 19:08:58 +0800

    添加组件

---
+++ components/tg-container.vue
@@ -0,0 +1,26 @@
+<template>
+  <div @click="handleClick" class="tg-container">
+    <slot></slot>
+  </div>
+</template>
+<script>
+    export default {
+        name: 'tg-container',
+        methods: {
+            handleClick(evt) {
+                this.$emit('click', evt);
+            }
+        }
+    };
+</script>
+<style lang="css">
+  .tg-container{
+    position: relative;
+  }
+
+  .tg-container:after{
+    content: "";
+    display: table;
+    clear: both;
+  }
+</style>
---
+++ components/tg-left-right.vue
@@ -0,0 +1,68 @@
+<template>
+  <div @click="handleClick" class="tg-left-right" :left="left" :right="right">
+    <slot name="left" :style="leftStyle"></slot>
+    <slot name="right" :style="rightStyle"></slot>
+  </div>
+</template>
+<script>
+    /**
+     * <tg-left-right>
+        <tg-container slot="left"></tg-container>
+        <tg-container slot="right"></tg-container>
+     </mt-layout-left-right>
+     */
+    export default {
+        name: 'tg-left-right',
+        props: {
+            left: String,
+            right: String
+        },
+        data: function() {
+            return {
+                leftStyle: '',
+                rightStyle: ''
+            };
+        },
+        methods: {
+            handleClick(evt) {
+                this.$emit('click', evt);
+            },
+            getOtherHalfWidth(width){
+                width = width.replace(/\s/g, '');
+                
+            }
+        },
+        created: function () {
+            let left = this.left;
+            let right = this.right;
+            if(left && right){
+                this.leftStyle = left;
+                this.rightStyle = right;
+            }else if(left){
+
+            }else if(right){
+
+            }else{
+                this.leftStyle = '50%';
+                this.rightStyle = '50%';
+            }
+        }
+    };
+
+</script>
+<style lang="css">
+  .tg-left-right{
+    position: relative;
+  }
+
+  .tg-left-right > *{
+    float: left;
+  }
+
+  .tg-left-right:after{
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+</style>
