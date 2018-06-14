80a8f179b578f5d0fcaa691eb2063cdc57d3e795
戚雨 <yuqi@wisedu.com>
2018-06-14 14:25:51 +0800

    分离所有的组件实现，只保留接口

--- entry.js
+++ entry.js
@@ -4,9 +4,6 @@ import axios from 'axios';
 import moment from 'moment';
 import FormConnectItem from './formConnector/FormConnectItem.js';
 
-import iviewForm from './formConnector/iview/form.js';
-import mintForm from './formConnector/mint/form.js';
-
 import {DataAdapter} from './DataBind/DataAdapter.js';
 import {DataSourceManager} from './DataBind/DataSourceManager.js';
 import DataFilter from './DataBind/DataFilter.js';
@@ -19,15 +16,11 @@ import TgLeftRight from './components/tg-left-right.vue';
 import TgLinkbutton from './components/tg-linkbutton.vue';
 import TgListview from './components/tg-listview.vue';
 import TgText from './components/tg-text.vue';
-import IviewFcForm from './formConnector/iview/iview-fc-form';
-import IviewFcStatic from './formConnector/iview/iview-fc-static';
-import MintFcForm from './formConnector/mint/mint-fc-form';
-import MintFcStatic from './formConnector/mint/mint-fc-static';
-const version = "1.6.12";
+const version = "1.7.0";
 const description = "turing components";
 const author = "金智教育 wisedu";
 const components = {
-    TgContainer,TgDiv,TgForm,TgImg,TgLeftRightItem,TgLeftRight,TgLinkbutton,TgListview,TgText,IviewFcForm,IviewFcStatic,MintFcForm,MintFcStatic
+    TgContainer,TgDiv,TgForm,TgImg,TgLeftRightItem,TgLeftRight,TgLinkbutton,TgListview,TgText
 };
 const install = function (Vue, opts = {}) {
     if (install.installed) return;
@@ -43,13 +36,11 @@ export default Object.assign({}, {
     install, 
     ...components,
     utils, version, description, author, 
-    DataAdapter, DataSourceManager, FormConnectItem, 
-    iview:{form:iviewForm}, mint:{form:mintForm}, DataFilter,
+    DataAdapter, DataSourceManager, FormConnectItem, DataFilter,
     axios, moment
 });
 export {
     utils, version, description, author, 
-    DataAdapter, DataSourceManager, FormConnectItem, 
-    iviewForm, mintForm, DataFilter,
+    DataAdapter, DataSourceManager, FormConnectItem, DataFilter,
     axios, moment
 }
